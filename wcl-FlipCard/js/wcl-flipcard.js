6679e3f7290c9e38d4dda40b21fe52f7036cc11a
Hirsz Tomasz <hirsz.tomasz@zepak.net>
2017-07-21 11:20:18 +0200

    update

--- wcl-FlipCard/js/wcl-flipcard.js
+++ wcl-FlipCard/js/wcl-flipcard.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
 
+    wclFlipCardInit();
 
 });
+
+function wclFlipCardInit() {
+    $('.wcl-flipcard-container').mouseover(function() {
+        $(this).addClass("wcl-flipcard-inversed");
+        $(this).removeClass("wcl-flipcard-normal");
+    });
+
+    $('.wcl-flipcard-container').mouseout(function() {
+        $(this).addClass("wcl-flipcard-normal");
+        $(this).removeClass("wcl-flipcard-inversed");
+    });
+}
